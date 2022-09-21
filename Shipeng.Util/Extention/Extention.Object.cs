d86b3c2c78ebea8e8a09c95066b37397d642b509
社会我鹏哥 <2382310075@qq.com>
2022-09-21 14:27:05 +0800

    1

--- Shipeng.Util/Extention/Extention.Object.cs
+++ Shipeng.Util/Extention/Extention.Object.cs
@@ -349,6 +349,23 @@ namespace Shipeng.Util
             return tmpVal;
         }
 
+        /// <summary>
+        /// 切割骆驼命名式字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        internal static string[] SplitCamelCase(this string str)
+        {
+            if (str == null) return Array.Empty<string>();
+
+            if (string.IsNullOrWhiteSpace(str)) return new string[] { str };
+            if (str.Length == 1) return new string[] { str };
+
+            return Regex.Split(str, @"(?=\p{Lu}\p{Ll})|(?<=\p{Ll})(?=\p{Lu})")
+                .Where(u => u.Length > 0)
+                .ToArray();
+        }
+
         #region 获取url
 
         public static string GetAbsoluteUri(this HttpRequest request)
